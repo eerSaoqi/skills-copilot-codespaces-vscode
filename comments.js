691e5b402403af993f7dc2678be19604d5017a9d
eerSaoqi <33rsaoqi@gmail.com>
2025-02-09 06:30:48 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,4 +1,4 @@
-// create web server
+// Create web server
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -12,5 +12,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-// run the server
-// node comments.js
